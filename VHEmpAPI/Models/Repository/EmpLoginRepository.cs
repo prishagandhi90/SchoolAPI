e51b37bfd081f07b644010720ed82527e408d420
harshilg <pd@venushospital.in>
2024-10-08 16:11:11 +0530

    otp template changed

--- VHEmpAPI/Models/Repository/EmpLoginRepository.cs
+++ VHEmpAPI/Models/Repository/EmpLoginRepository.cs
@@ -31,51 +31,147 @@ namespace VHEmpAPI.Models.Repository
             return (IEnumerable<IsValidData>)Enumerable.Empty<string>();
         }
 
+        //public async Task<IEnumerable<CommonProcOutputFields.OTP>> SendEmpMobileOTP(OTP respOTP)
+        //{
+        //    try
+        //    {
+        //        string sqlStr = "exec dbo.Send_EMP_MobileOTP @p_MobileNo = '"+ respOTP.MobileNo +"' ";
+        //        var otp = await AppDbContextAdm.OTP.FromSqlRaw(sqlStr).ToListAsync();
+        //        string SMSOtp = "", Message = "";
+        //        if (otp != null && otp.Count() > 0)
+        //        {
+        //            SMSOtp = otp.Select(x => x.OTPNo).ToList()[0].ToString();
+        //        }
+
+        //        //Message = "Mobile number verification code is " + SMSOtp + ". Kindly share it with our hospital staff to complete the registration / admission / billing process. Stay Safe and Healthy. Team Venus Hospital, Surat.";
+
+        //        //var client = new RestClient("http://login.bulksmsgateway.in/sendmessage.php");
+        //        //var request = new RestRequest();
+        //        //request.AddQueryParameter("user", "Venush");
+        //        //request.AddQueryParameter("password", "vh*1600VH@");
+        //        //request.AddQueryParameter("sender", "VENHOS");
+        //        //request.AddQueryParameter("mobile", respOTP.MobileNo);
+        //        //request.AddQueryParameter("type", "3");
+        //        //request.AddQueryParameter("message", Message);
+        //        //request.AddQueryParameter("template_id", "1407166029670465803");
+
+        //        //ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+        //        //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        //        //RestResponse response = await client.GetAsync(request);
+        //        //if (response.StatusCode == HttpStatusCode.OK)
+        //        //{
+        //        //    var SMSResult = JsonConvert.DeserializeObject<OTP>(response.Content);
+        //        //    if (SMSResult.Status.ToLower() == "failed")
+        //        //    {
+        //        //        List<OTP> list = new List<OTP>();
+        //        //        respOTP.Status = "failed";
+        //        //        respOTP.OTPNo = "";
+        //        //        list.Add(respOTP);
+        //        //        return list;
+        //        //    }
+        //        //    return otp;
+        //        //}
+        //        //else
+        //        //{
+        //        //    return otp;
+        //        //    throw new Exception(response?.ErrorMessage);
+        //        //}
+        //        return otp;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return (IEnumerable<OTP>)Enumerable.Empty<string>();
+        //    }
+        //    return (IEnumerable<OTP>)Enumerable.Empty<string>();
+        //}
+
         public async Task<IEnumerable<CommonProcOutputFields.OTP>> SendEmpMobileOTP(OTP respOTP)
         {
             try
             {
-                string sqlStr = "exec dbo.Send_EMP_MobileOTP @p_MobileNo = '"+ respOTP.MobileNo +"' ";
+                string sqlStr = "exec dbo.Send_EMP_MobileOTP @p_MobileNo = '" + respOTP.MobileNo + "' ";
                 var otp = await AppDbContextAdm.OTP.FromSqlRaw(sqlStr).ToListAsync();
-                string SMSOtp = "", Message = "";
+                string SMSOtp = "";
+                string Message = "";
                 if (otp != null && otp.Count() > 0)
                 {
                     SMSOtp = otp.Select(x => x.OTPNo).ToList()[0].ToString();
                 }
 
-                //Message = "Mobile number verification code is " + SMSOtp + ". Kindly share it with our hospital staff to complete the registration / admission / billing process. Stay Safe and Healthy. Team Venus Hospital, Surat.";
+                //if (respOTP.MobileNo == "9429728770")
+                if (respOTP.MobileNo == "9429728770" || respOTP.MobileNo == "9905475111" || respOTP.MobileNo == "9316689895")
+                {
+                    Message = $"Greetings from Venus Hospital!\n{{0}} is your OTP to log in to VENUS HOSPITAL account and it is valid for {{1}} minutes. Do not share it with anyone for security reasons.\n\nStay Safe and Healthy!\nTeam Venus Hospital.";
+                    int validityMinutes = 20;
+                    string formattedMessage = string.Format(Message, SMSOtp, validityMinutes);
 
-                //var client = new RestClient("http://login.bulksmsgateway.in/sendmessage.php");
-                //var request = new RestRequest();
-                //request.AddQueryParameter("user", "Venush");
-                //request.AddQueryParameter("password", "vh*1600VH@");
-                //request.AddQueryParameter("sender", "VENHOS");
-                //request.AddQueryParameter("mobile", respOTP.MobileNo);
-                //request.AddQueryParameter("type", "3");
-                //request.AddQueryParameter("message", Message);
-                //request.AddQueryParameter("template_id", "1407166029670465803");
+                    var client = new RestClient("http://login.bulksmsgateway.in/sendmessage.php");
+                    var request = new RestRequest();
+                    request.AddQueryParameter("user", "Venush");
+                    request.AddQueryParameter("password", "vh*1600VH@");
+                    request.AddQueryParameter("sender", "VENHOS");
+                    request.AddQueryParameter("mobile", respOTP.MobileNo);
+                    request.AddQueryParameter("type", "3");
+                    request.AddQueryParameter("message", formattedMessage);
+                    request.AddQueryParameter("template_id", "1407172449585399052");
 
-                //ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
-                //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-                //RestResponse response = await client.GetAsync(request);
-                //if (response.StatusCode == HttpStatusCode.OK)
-                //{
-                //    var SMSResult = JsonConvert.DeserializeObject<OTP>(response.Content);
-                //    if (SMSResult.Status.ToLower() == "failed")
-                //    {
-                //        List<OTP> list = new List<OTP>();
-                //        respOTP.Status = "failed";
-                //        respOTP.OTPNo = "";
-                //        list.Add(respOTP);
-                //        return list;
-                //    }
-                //    return otp;
-                //}
-                //else
-                //{
-                //    return otp;
-                //    throw new Exception(response?.ErrorMessage);
-                //}
+                    ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+                    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                    RestResponse response = await client.GetAsync(request);
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        var SMSResult = JsonConvert.DeserializeObject<OTP>(response.Content);
+                        //if (SMSResult.Status.ToLower() == "failed")
+                        //{
+                        //    List<OTP> list = new List<OTP>();
+                        //    respOTP.Status = "failed";
+                        //    respOTP.OTPNo = "";
+                        //    list.Add(respOTP);
+                        //    return list;
+                        //}
+                        return otp;
+                    }
+                    else
+                    {
+                        return otp;
+                        throw new Exception(response?.ErrorMessage);
+                    }
+
+                    //Below Old working template
+                    //Message = "Mobile number verification code is " + SMSOtp + ". Kindly share it with our hospital staff to complete the registration / admission / billing process. Stay Safe and Healthy. Team Venus Hospital, Surat.";
+                    //var client = new RestClient("http://login.bulksmsgateway.in/sendmessage.php");
+                    //var request = new RestRequest();
+                    //request.AddQueryParameter("user", "Venush");
+                    //request.AddQueryParameter("password", "vh*1600VH@");
+                    //request.AddQueryParameter("sender", "VENHOS");
+                    //request.AddQueryParameter("mobile", respOTP.MobileNo);
+                    //request.AddQueryParameter("type", "3");
+                    //request.AddQueryParameter("message", formattedMessage);
+                    //request.AddQueryParameter("template_id", "1407166029670465803");
+
+                    //ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+                    //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                    //RestResponse response = await client.GetAsync(request);
+                    //if (response.StatusCode == HttpStatusCode.OK)
+                    //{
+                    //    var SMSResult = JsonConvert.DeserializeObject<OTP>(response.Content);
+                    //    //if (SMSResult.Status.ToLower() == "failed")
+                    //    //{
+                    //    //    List<OTP> list = new List<OTP>();
+                    //    //    respOTP.Status = "failed";
+                    //    //    respOTP.OTPNo = "";
+                    //    //    list.Add(respOTP);
+                    //    //    return list;
+                    //    //}
+                    //    return otp;
+                    //}
+                    //else
+                    //{
+                    //    return otp;
+                    //    throw new Exception(response?.ErrorMessage);
+                    //}
+                }
+
                 return otp;
             }
             catch (Exception ex)
